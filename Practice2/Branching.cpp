b1f13f95aed24677b3234d97a3b016a02fbda59e
Te4nick <43699293+Te4nick@users.noreply.github.com>
2021-09-18 18:04:02 +0300

    Create Branching.cpp

---
+++ Practice2/Branching.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float x, a;
+    cout << "Enter x, a: ";
+    cin >> x; cin >> a;
+    cout << endl;
+    if (abs(x) < 1) {
+        if (x == 0) cout << "Error: natural log from 0\n";
+        else {
+            float w = a * log(abs(x));
+            cout << w;
+        }
+    }
+    else {
+        if (a - x*x < 0) cout << "Error: square root from a negative number\n";
+        else {
+            float w = sqrt(a - x*x);
+            cout << w;
+        }
+    }
+    return 0;
+}
